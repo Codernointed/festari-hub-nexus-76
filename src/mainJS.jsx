9598b3631da8f00a8aa5c3ded012b71e4fa1f5a9
ethenkem <ethenatx@gmail.com>
2025-03-27 19:45:09 +0000

    revertion

--- src/mainJS.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppJS from './AppJS'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <AppJS />
-)
